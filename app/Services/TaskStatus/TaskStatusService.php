2911f876a70be25e6f62b4421b12d5b0194d69fd
babaAH <shama_chich@icloud.com>
2021-08-29 05:32:33 +0300

    AC for TaskStatus model && fix permission seeder

--- app/Services/TaskStatus/TaskStatusService.php
+++ app/Services/TaskStatus/TaskStatusService.php
@@ -5,13 +5,24 @@ namespace App\Services\TaskStatus;
 use DB;
 use App\Models\TaskStatus;
 use App\Services\Task\TaskServiceInterface;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\Finder\Exception\AccessDeniedException;
 
 class TaskStatusService implements TaskStatusServiceInterface
 {
 
     public function create($data)
     {
+        if(!$user = Auth::user()){
+            throw new AuthenticationException();
+        }
+
+        if(!$user->hasPermission("create-task-status")){
+            throw new AccessDeniedException("Недостаточно прав для этого действия");
+        }
+
         try {
             DB::beginTransaction();
             $task_status = new TaskStatus();
@@ -28,6 +39,14 @@ class TaskStatusService implements TaskStatusServiceInterface
 
     public function update($data, $id)
     {
+        if(!$user = Auth::user()){
+            throw new AuthenticationException();
+        }
+
+        if(!$user->hasPermission("update-task-status")){
+            throw new AccessDeniedException("Недостаточно прав для этого действия");
+        }
+
         $task_status = TaskStatus::find($id);
 
         if(is_null($task_status)){
@@ -52,6 +71,14 @@ class TaskStatusService implements TaskStatusServiceInterface
      */
     public function index()
     {
+        if(!$user = Auth::user()){
+            throw new AuthenticationException();
+        }
+
+        if(!$user->hasPermission("read-task-status")){
+            throw new AccessDeniedException("Недостаточно прав для этого действия");
+        }
+
         return TaskStatus::orderBy("id", "desc")->paginate();
     }
 
@@ -62,6 +89,14 @@ class TaskStatusService implements TaskStatusServiceInterface
      */
     public function show(int $id)
     {
+        if(!$user = Auth::user()){
+            throw new AuthenticationException();
+        }
+
+        if(!$user->hasPermission("read-task-status")){
+            throw new AccessDeniedException("Недостаточно прав для этого действия");
+        }
+
         $task_status = TaskStatus::find($id);
 
         if($task_status){
@@ -73,6 +108,14 @@ class TaskStatusService implements TaskStatusServiceInterface
 
     public function delete($id)
     {
+        if(!$user = Auth::user()){
+            throw new AuthenticationException();
+        }
+
+        if(!$user->hasPermission("update-task-status")){
+            throw new AccessDeniedException("Недостаточно прав для этого действия");
+        }
+
         //TODO
     }
 }
